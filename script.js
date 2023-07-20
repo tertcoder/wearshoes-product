79f7344ff833cde65df0e6395afa16d888f9f794
tercoder <tuyishimirebt12@gmail.com>
2023-07-20 23:27:51 +0200

    initial user interface

---
+++ script.js
@@ -0,0 +1,11 @@
+const products = [
+  {
+    id: 01,
+    name: "Fun Air Jordan For Men",
+    image: "./images/pic-1.webp",
+    brand: "Nike brand",
+    rating: 4.5,
+    price: 150,
+    new: true,
+  },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
