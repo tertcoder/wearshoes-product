d1aadb1ede1ad957ac145752a8f3671529fa96a8
tercoder <tuyishimirebt12@gmail.com>
2023-07-21 00:37:37 +0200

    initializing again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      sm1: "680px",
+      md: "780px",
+      md1: "868px",
+      lg: "976px",
+      xl: "1440px",
+    },
     fontFamily: {
-      sans: ["Open Sans", "sans-serif"],
+      sans: ["Poppins", "sans-serif"],
+      title: ["Coiny", "cursive"],
     },
 
     extend: {},
